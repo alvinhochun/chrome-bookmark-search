b6d9317ec1a6c8a49b3830e41f11bb92d10d5a2c
Alvin Wong <alvinhochun@gmail.com>
2013-02-23 16:20:27 +0800

    Options page now automatically display the correct version

--- src/options.js
+++ src/options.js
@@ -51,4 +51,10 @@ window.addEventListener("load", function(){
 		e.preventDefault();
 		save_options();
 	});
+	var eles = document.getElementsByName("version");
+	var len = eles.length;
+	var vc = "v" + chrome.runtime.getManifest().version;
+	for(var i = 0; i < len; i++){
+		eles[i].textContent = vc;
+	}
 });
